4aa50dbe95d711410b75161da319a99634a861ae
Anurag Nampally <anurag.nampally@gmail.com>
2023-10-12 13:31:33 -0400

    Added Pdf's and Questions for OCT 12

--- Intro to JS 2 - OCT - 2023/questions.js
+++ Intro to JS 2 - OCT - 2023/questions.js
@@ -97,3 +97,124 @@ function removeComposites(N, arr) {
   }
   return res;
 }
+
+// Count words in a string
+function countWords(s) {
+  const arr = s.split(" ");
+  let words = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length != 0) {
+      words++;
+    }
+  }
+
+  return words;
+}
+
+// Toggle Characters
+// link : https://course.acciojob.com/idle?question=4ed3641c-3947-40e1-a296-114067186afe
+function toggleCase(n, str) {
+  let ans = "";
+  for (let i = 0; i < n; i++) {
+    if ("A" <= str[i] && str[i] <= "Z") {
+      ans += str[i].toLowerCase();
+    } else {
+      ans += str[i].toUpperCase();
+    }
+  }
+  console.log(ans);
+}
+
+function toggleCase(n, str) {
+  let ans = "";
+  for (let i = 0; i < n; i++) {
+    const ch = str[i];
+    const lower = str[i].toLowerCase();
+    const upper = str[i].toUpperCase();
+    if (ch == lower) {
+      ans += upper;
+    } else {
+      ans += lower;
+    }
+  }
+  console.log(ans);
+}
+
+// Pangrams
+// link: https://course.acciojob.com/idle?question=7eee469e-1a6d-4b5f-bbe0-a31fdc15e47d
+function isPangram(sentence) {
+  sentence = sentence.toLowerCase();
+  for (let ascii = 97; ascii <= 122; ascii++) {
+    const ch = String.fromCharCode(ascii);
+    if (sentence.includes(ch) == false) {
+      return "not pangram";
+    }
+  }
+  return "pangram";
+}
+
+// Camel Case
+// link: https://course.acciojob.com/idle?question=a39547be-677e-48bd-b068-ee49f6fb8e25
+function camelCase(s) {
+  const allWords = s.split("_");
+  for (let i = 1; i < allWords.length; i++) {
+    const word = allWords[i];
+    allWords[i] = word[0].toUpperCase() + word.slice(1);
+  }
+  return allWords.join("");
+}
+
+function camelCase(s) {
+  const allWords = s.split("_");
+  let ans = allWords[0];
+  for (let i = 1; i < allWords.length; i++) {
+    const word = allWords[i];
+    ans += word.replace(word[0], word[0].toUpperCase());
+  }
+  return ans;
+}
+
+// Palindrome String
+// link: https://course.acciojob.com/idle?question=598cc264-4776-408f-a80b-4d156d9e8709
+function isPalindrome(s) {
+  s = s.toLowerCase();
+  let start = 0;
+  let end = s.length - 1;
+
+  while (start <= end) {
+    if (s[start] < "a" || s[start] > "z") {
+      start++;
+    } else if (s[end] < "a" || s[end] > "z") {
+      end--;
+    } else if (s[start] != s[end]) {
+      return 0;
+    } else {
+      start++;
+      end--;
+    }
+  }
+
+  return 1;
+}
+// TC : O(N), SC: O(1)
+
+// Compressed Strings
+// link: https://course.acciojob.com/idle?question=d0efdf7e-f990-49d7-bbdc-8f4c5eb37de1
+function compressString(s) {
+  let cnt = 1;
+  let ans = "";
+  const n = s.length;
+  for (let i = 1; i < n; i++) {
+    if (s[i] == s[i - 1]) {
+      cnt++; // same region increment cnt
+    } else {
+      ans += s[i - 1] + (cnt > 1 ? cnt : ""); // prev_char + cnt
+      cnt = 1; // re-initialize as you entered new region
+    }
+  }
+
+  // missed adding last exploration
+  ans += s[n - 1] + (cnt > 1 ? cnt : "");
+  return ans;
+}
+// TC: O(N), SC: O(1) (if you want to consider output => O(N))
