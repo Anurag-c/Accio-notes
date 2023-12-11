de1a46b1b266efca38c63e28ad3d89bfe198c0c4
Anurag Nampally <anurag.nampally@gmail.com>
2023-12-11 12:37:04 -0500

    Added notes for dec 11

--- Intro to JS 1 - DEC - 2023/script.js
+++ Intro to JS 1 - DEC - 2023/script.js
@@ -1,4 +1,7 @@
 /*
+
+///////////////////////// DEC 7th ///////////////////////////////////
+
 // Printing Values
 console.log("Hello world"); // String
 console.log("Hello I am Anurag, 22 years old"); // String
@@ -37,10 +40,11 @@ let isJavaScriptFun = "YESS!!!"
 -> this is redeclaring a same variable which will give
 an error because you cannot have 2 varibles with same name.
 
-
 isJavaScriptFun = "YES!!!"; // changing the box value
 console.log(isJavaScriptFun);
 console.log(typeof isJavaScriptFun);
+
+//////////////////////////// DEC 8th ////////////////////////////
 
 // variable names cannot start with a number
 let 1num = 12;
@@ -187,4 +191,177 @@ console.log(a, b);
 // always use brackets to avoid confusion around precedence
 const average = (ageAnurag + ageAbbas) / 2;
 console.log(average);
+
+//////////////////////////// DEC 11th ////////////////////////////
+
+// Type Conversion
+const birthYear = "2001.05";
+
+// What will be the birth year after 22 years ?
+console.log(Number(birthYear), birthYear);
+console.log(Number(birthYear) + 22);
+console.log(parseInt(birthYear) + 22); // fractional part is lost
+
+console.log(String(23), 23);
+console.log(Number("2%3@$csv6abc7")); // NaN
+console.log(typeof NaN); // number
+
+// false => similar to Boolean(empty)
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(""));
+console.log(Boolean(NaN));
+
+// remaining all are true
+console.log(Boolean(1));
+console.log(Boolean("anurag"));
+
+
+// add string with a number without converting
+// type coercion
+console.log("I am " + 22 + " years old");
+console.log("23" + 10); // 2310
+console.log("23" + 10 + 5); // 23105
+console.log("23" - "10" - 3); // 10
+console.log("23" / "2"); // 11.5
+console.log("23" * 2); // 46
+
+let n = "1" + 1;
+console.log(n, typeof n);
+n--;
+console.log(n, typeof n);
+
+
+// String template literals
+const fname = "Anurag";
+const birthYear = 2001;
+const job = "teacher";
+
+const anurag =
+  "I am " + fname + ", a " + job + ", " + (2023 - birthYear) + " years old";
+console.log(anurag);
+
+const anuragNew = `I am ${fname}, a ${job}, ${2023 - birthYear} years old`;
+console.log(anuragNew);
+console.log(`I am ${fname}`);
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("You can apply for a driver's license");
+}
+
+console.log("outside of 1st if-block");
+
+if (age >= 18) {
+  console.log("Hurray I got my license");
+}
+
+console.log("outside of 2nd if-block");
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("I can apply for drivers license");
+} else {
+  console.log("cannot take drivers license");
+}
+console.log("outside if-else");
+
+const day = "tuesday";
+if (day == "monday") {
+  console.log("I have to go to movie");
+} else {
+  console.log("I do not know what to do");
+}
+if (day == "monday") {
+  console.log("I have to go for shopping");
+}
+if (day == "tuesday") {
+  console.log("I have an exam");
+  console.log("I have to attend a wedding");
+} else {
+  console.log("I do not know what to do");
+}
+console.log("outside if-else");
+
+// scope
+const birthYear = 2001;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
+
+// else if statements
+const day = "";
+
+if (day == "monday") {
+  console.log("Plan course structure");
+} else if (day == "tuesday") {
+  console.log("Prepare for exams");
+} else if (day == "wednesday") {
+  console.log("Write examples for coding lectures");
+} else if (day == "thursday") {
+  console.log("Watch recordings");
+} else if (day == "friday") {
+  console.log("solve assignments");
+} else if (day == "saturday") {
+  console.log("Revise all notes");
+} else if (day == "sunday") {
+  console.log("attempt contest");
+} else {
+  console.log("Please enter a valid day");
+}
+
+// logical operators
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision); // AND
+console.log(hasDriversLicense || hasGoodVision); // OR
+console.log(!hasDriversLicense); // NOT
+console.log(!hasGoodVision);
+
+if (hasDriversLicense && hasGoodVision) {
+  console.log("Yayyy !!!!, I am able to drive");
+} else {
+  console.log("Sorry you cannot drive");
+}
+
+/*
+BMI = (MASS / HEIGHT^2)
+1. Print a nice output to the console, saying who has the higher BMI.
+The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+
+2. Use a template literal to include the BMI values in the outputs. 
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// you can calculate square(height) in either ways
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn); // brackets are imp
+
+if (bmiMark > bmiJohn) {
+  console.log(
+    `Mark's BMI (${bmiMark.toFixed(
+      1
+    )}) is higher than John's (${bmiJohn.toFixed(1)})!`
+  );
+} else {
+  console.log(
+    `John's BMI (${bmiJohn.toFixed(
+      1
+    )}) is higher than Mark's (${bmiMark.toFixed(1)})!`
+  );
+}
 */
+
+//////////////////////////// DEC 12th ////////////////////////////
