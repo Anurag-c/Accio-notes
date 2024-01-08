65103da1b7ca029130ea242ed0f1734881aa576d
Anurag Nampally <anurag.nampally@gmail.com>
2024-01-08 12:25:15 -0500

    Jan 8 Notes added

---
+++ Intro to JS 1 - JAN - 2024/script.js
@@ -0,0 +1,79 @@
+/////////////////////////////////////////////////// JAN 8 ///////////////////////////////////////////
+
+// How to print to console / output
+console.log("Hi I am Anurag");
+console.log("Hello world");
+console.log(234);
+console.log("234");
+
+// How to create a variable ?
+let firstName = "Anurag";
+console.log("Anurag");
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable Naming conventions and rules
+// 1. variable names cannot start with number
+/*
+let 3years = "three years";
+console.log(3years);
+*/
+
+// 2. It can only have letters, numbers, dollar, underscore
+let first_name = "Anurag";
+let first$name = "Anurag";
+let first123name = "Anurag";
+let first_name_1$2$3 = "Anurag";
+
+console.log(first_name);
+console.log(first$name);
+console.log(first123name);
+console.log(first_name_1$2$3);
+
+// 3. Try to follow camelcase
+// (first letter is small, all other first letters
+// of a word are capital)
+let placeOfBirth = "India";
+console.log(placeOfBirth);
+
+let birthYear = 2001;
+console.log(birthYear);
+
+// 4. you cannot use reserved keywords
+/*
+let let = "Anurag";
+console.log(let);
+
+let function = "Anurag";
+console.log(function);
+*/
+
+// 5. use all capitals for constants
+let PI = 3.14;
+console.log(PI);
+
+let DATABASE_URL = "https://acciodb-external-ind-west.com/students";
+console.log(DATABASE_URL);
+
+// 6. Meaningful variable names (V.IMP)
+let a = "Software Engineer";
+let x = "Teacher";
+console.log(a);
+console.log(x);
+
+let job1 = "Software Engineer";
+let job2 = "Teacher";
+console.log(job1);
+console.log(job2);
+
+let prevJob = "Software Engineer";
+let currJob = "Teacher";
+console.log(prevJob);
+console.log(currJob);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////// JAN 9 ///////////////////////////////////////////
