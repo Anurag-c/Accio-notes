c60629eb23a1f60fdc9fc3e03a9603d265f314d4
Anurag Nampally <anurag.nampally@gmail.com>
2024-02-08 12:26:41 -0500

    Added notes for feb 8

---
+++ Intro to JS 1 - FEB - 2024/script.js
@@ -0,0 +1,28 @@
+// The below code explains how to output to console
+/*
+comment1
+comment2
+comment3
+comment4
+*/
+
+console.log("Hello world !!!");
+console.log("Hi I am Anurag");
+console.log("12345678");
+console.log(12345678);
+console.log("1a2b3c");
+
+// How to create a variable
+let firstName =
+  "Anuragajdsljaslkjdasdnoansdoasnoinfoanfoinsdivnidvjbcvbdbghihfisdkncsivusdhgidngfjk jhdfuigheihkfnsdokfhoisdhgoibwejghudvnisdighewt9iesfoasfjoasfe9wur";
+
+// you reuse whenever you want
+console.log(firstName);
+
+// 1000 lines code.....
+
+console.log(firstName);
+
+// 2000 lines code....
+
+console.log(firstName);
