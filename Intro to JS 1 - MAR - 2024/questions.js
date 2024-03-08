85726bfb8c96f44282b38013ed64f3b886cbc762
Anurag Nampally <anurag.nampally@gmail.com>
2024-03-08 11:34:55 -0500

    Added notes on Mar 8

---
+++ Intro to JS 1 - MAR - 2024/questions.js
@@ -0,0 +1,17 @@
+// Welcome To JavaScript
+// Link: https://course.acciojob.com/idle?question=c931523a-97aa-428c-a285-6fce277688a4
+console.log("Hello, World.");
+console.log("Hello, JavaScript.");
+
+// Variable Practice Question 1
+// Link: https://course.acciojob.com/idle?question=2180cf80-db05-4bb0-894d-d46967d5f251
+console.log("My num is 10");
+console.log("My num is", 10);
+let a = 10;
+console.log("My num is", a);
+
+// Variable Practice Question 2
+// Link: https://course.acciojob.com/idle?question=d7f01721-b073-4270-b4e3-d0629d5550d1
+let a = 10.45;
+a = 20.55;
+console.log(a);
---
+++ Intro to JS 1 - MAR - 2024/script.js
@@ -0,0 +1,62 @@
+// Values
+console.log("Hi I am Anurag");
+console.log("Hello world");
+console.log(12345);
+console.log("12345");
+console.log("Hi I am Anurag", 23, "years old", "xyz", "abc");
+console.log("shivya", "shalini", "kanishk", "omkar");
+console.log("shivya shalini kanishk omkar");
+
+// You can store values in a variable
+// How to create variable ?
+let firstName = "Anurag";
+console.log(firstName);
+
+let age = 18;
+console.log(age);
+
+let gender = "male";
+console.log(gender);
+
+let birthYear = 2001;
+console.log(birthYear);
+
+// Always give meaningful variable names
+let a = "software engineer";
+let x = "teacher";
+console.log(a);
+console.log(x);
+
+let prevJob = "software engineer";
+let currJob = "teacher";
+console.log(prevJob);
+console.log(currJob);
+
+// You cannot have two variables with same name
+// let age = 18;
+// let age = 20;
+
+// You can change the values of a variable
+// one variable can have different values
+let superHero = "Hulk";
+console.log(superHero);
+superHero = "Iron Man";
+console.log(superHero);
+superHero = "Spider Man";
+console.log(superHero);
+superHero = 1234;
+console.log(superHero);
+
+// you can declare multiple variables in a single line
+
+/*
+let p = 1;
+let q = 2;
+let r = 3;
+console.log(p, q, r);
+*/
+
+let p = 1,
+  q = "two",
+  r = 3;
+console.log(p, q, r);
