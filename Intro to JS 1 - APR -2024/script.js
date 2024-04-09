f0c7aef3a6414f800b6be01278ba3ce757a8c536
Anurag Nampally <anurag.nampally@gmail.com>
2024-04-09 13:26:36 -0400

    added notes for apr 9

---
+++ Intro to JS 1 - APR -2024/script.js
@@ -0,0 +1,26 @@
+// How to print to console/output
+// values
+console.log("Hello world");
+console.log("Hi Anurag", "Hello Annu", "How are you Tarun");
+
+console.log("2024");
+console.log(2024); // numbers dont need double quotes
+
+// variables
+let firstName = "Anurag";
+let job = "Teacher";
+console.log(firstName);
+console.log(job);
+// console.log(lastName); // not defined error
+
+// you can change the value of a variable
+console.log(firstName);
+firstName = "Shashank";
+console.log(firstName);
+
+console.log(job);
+job = "Student";
+console.log(job);
+
+console.log(firstName);
+console.log(job);
